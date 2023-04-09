03b2ce7ebf61b5ed198b337473f20abaa6a96574
Stephen Hodgson <hodgson.designs@gmail.com>
2023-04-09 12:13:16 -0400

    com.utilities.extensions 1.0.1 (#2)
    - misc cleanup to scriptable object ex
    - added SerializedObjectExtensions

---
+++ Utilities.Extensions/Packages/com.utilities.extensions/Editor/SerializedObjectExtensions.cs
@@ -0,0 +1,12 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+
+namespace Utilities.Extensions.Editor
+{
+    public static class SerializedObjectExtensions
+    {
+        public static bool IsNull(this SerializedObject @object)
+            => @object == null || @object.targetObject == null;
+    }
+}
