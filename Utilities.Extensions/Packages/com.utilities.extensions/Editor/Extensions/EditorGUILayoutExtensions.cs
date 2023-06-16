afd4e72a8950cd4a46f36db24d3582e00c080a74
Stephen Hodgson <hodgson.designs@gmail.com>
2023-06-16 16:06:48 -0400

    final touches to serialized dictionary property drawer

--- Utilities.Extensions/Packages/com.utilities.extensions/Editor/Extensions/EditorGUILayoutExtensions.cs
+++ Utilities.Extensions/Packages/com.utilities.extensions/Editor/Extensions/EditorGUILayoutExtensions.cs
@@ -1,5 +1,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,6 +20,92 @@ namespace Utilities.Extensions.Editor
             rect.height = height;
             color ??= EditorGUIUtility.isProSkin ? new Color(0.1f, 0.1f, 0.1f) : new Color(0.5f, 0.5f, 0.5f, 1);
             EditorGUI.DrawRect(rect, color.Value);
+        }
+
+        private static readonly Dictionary<string, Vector2> scrollCache = new Dictionary<string, Vector2>();
+
+        private static MethodInfo scrollableTextAreaInternal;
+
+        private static MethodInfo ScrollableTextAreaInternal
+        {
+            get
+            {
+                if (scrollableTextAreaInternal != null) { return scrollableTextAreaInternal; }
+                var types = new[]
+                {
+                    typeof(Rect),
+                    typeof(string),
+                    typeof(Vector2).MakeByRefType(),
+                    typeof(GUIStyle)
+                };
+                return scrollableTextAreaInternal = typeof(EditorGUI)
+                    .GetMethod("ScrollableTextAreaInternal", BindingFlags.Static | BindingFlags.NonPublic, null, types, null);
+            }
+        }
+
+        public static void DrawTextArea(Rect rect, SerializedProperty property)
+        {
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                scrollCache.TryAdd(property.propertyPath, Vector2.zero);
+                var parameters = new object[]
+                {
+                rect,
+                property.stringValue,
+                scrollCache[property.propertyPath],
+                EditorStyles.textArea
+                };
+                property.stringValue = (string)ScrollableTextAreaInternal.Invoke(null, parameters);
+                scrollCache[property.propertyPath] = (Vector2)parameters[2];
+            }
+            else
+            {
+                EditorGUI.LabelField(rect, $"Use {nameof(DrawTextArea)} with string.");
+            }
+        }
+
+        private static Dictionary<string, TextAreaHeight> cachedTextAreaHeights = new Dictionary<string, TextAreaHeight>();
+
+        private class TextAreaHeight
+        {
+            public TextAreaHeight(GUIContent content, int height)
+            {
+                this.content = content;
+                this.height = height;
+            }
+
+            public GUIContent content;
+            public float height;
+        }
+
+        public static float GetTextAreaHeight(SerializedProperty property, float width, int minLines = 1, int maxLines = 12)
+        {
+            property.serializedObject.Update();
+            if (string.IsNullOrWhiteSpace(property.stringValue))
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+
+            const float lineHeight = 13f;
+            cachedTextAreaHeights.TryAdd(property.propertyPath, new TextAreaHeight(new GUIContent(property.stringValue), 0));
+            var cachedContent = cachedTextAreaHeights[property.propertyPath].content;
+            var cachedHeight = cachedTextAreaHeights[property.propertyPath].height;
+            var textAreaHeight = EditorStyles.textArea.CalcHeight(cachedContent, width);
+            var baseTextAreaHeight = EditorGUIUtility.singleLineHeight;
+            var result = baseTextAreaHeight + ((Mathf.Clamp(Mathf.CeilToInt(textAreaHeight / lineHeight), minLines, maxLines) - 1) * lineHeight);
+
+            if (!Mathf.Approximately(result, cachedHeight))
+            {
+                if (cachedHeight > 0 &&
+                    property.stringValue.Equals(cachedContent.text))
+                {
+                    return cachedHeight;
+                }
+            }
+
+            cachedTextAreaHeights[property.propertyPath].content.text = property.stringValue;
+            cachedTextAreaHeights[property.propertyPath].height = result;
+            return result;
         }
     }
 }
--- Utilities.Extensions/Packages/com.utilities.extensions/Editor/PropertyDrawers/SerializedDictionaryPropertyDrawer.cs
+++ Utilities.Extensions/Packages/com.utilities.extensions/Editor/PropertyDrawers/SerializedDictionaryPropertyDrawer.cs
@@ -137,13 +137,13 @@ namespace Utilities.Extensions.Editor
             {
                 if (!reorderableListCache.TryGetValue(property.propertyPath, out var list))
                 {
-                    list = new ReorderableList(serializedDictionary.ToList(), typeof(KeyValuePair<,>), true, true, true, true);
+                    list = new ReorderableList(serializedDictionary.ToList(), typeof(KeyValuePair<,>), false, true, true, true);
                     list.drawHeaderCallback += rect => { EditorGUI.LabelField(rect, label); };
                     list.drawElementCallback += (rect, index, active, focused) =>
                     {
                         OnListDrawElementCallback(rect, index, active, focused, serializedDictionary);
                     };
-
+                    list.elementHeightCallback += index => OnListElementHeightCallback(index, serializedDictionary);
                     list.onCanAddCallback += _ => serializedDictionary.CanAddNewItem();
                     list.onAddCallback += reorderableList =>
                     {
@@ -167,7 +167,7 @@ namespace Utilities.Extensions.Editor
         private static void OnListDrawElementCallback(Rect rect, int index, bool active, bool focused, SerializedDictionaryObject serializedDictionary)
         {
             serializedDictionary.UpdateSerializedObject();
-            var item = serializedDictionary.GetArrayElementAtIndex(index);
+            var (key, value) = serializedDictionary.GetArrayElementAtIndex(index);
 
             if (focused || active)
             {
@@ -175,24 +175,44 @@ namespace Utilities.Extensions.Editor
             }
 
             EditorGUI.BeginChangeCheck();
-            var keyRect = new Rect(rect)
-            {
-                width = rect.width * 0.45f
-            };
 
-            EditorGUI.PropertyField(keyRect, item.Key, GUIContent.none);
-            var valueRect = new Rect(rect)
-            {
-                width = rect.width * 0.55f,
-                position = new Vector2(rect.x + keyRect.width + 4, rect.y)
-            };
+            var lineHeight = EditorGUIUtility.singleLineHeight;
+            var keyRect = new Rect(rect.x, rect.y, rect.width, lineHeight);
+            var valueHeight = EditorGUILayoutExtensions.GetTextAreaHeight(value, rect.width);
+            var valueRect = new Rect(rect.x, rect.y + lineHeight + EditorGUIUtility.standardVerticalSpacing, rect.width, valueHeight);
 
-            EditorGUI.PropertyField(valueRect, item.Value, GUIContent.none);
+            EditorGUI.PropertyField(keyRect, key, GUIContent.none);
+
+            if (value.propertyType == SerializedPropertyType.String)
+            {
+                EditorGUILayoutExtensions.DrawTextArea(valueRect, value);
+            }
+            else
+            {
+                EditorGUI.PropertyField(valueRect, value, GUIContent.none);
+            }
 
             if (EditorGUI.EndChangeCheck())
             {
                 serializedDictionary.ApplyModifiedProperties();
             }
+        }
+
+        private static float OnListElementHeightCallback(int index, SerializedDictionaryObject serializedDictionary)
+        {
+            serializedDictionary.UpdateSerializedObject();
+            var (key, value) = serializedDictionary.GetArrayElementAtIndex(index);
+
+            // Calculate heights taking into account multi-line serialized properties for value property
+            var keyHeight = EditorGUI.GetPropertyHeight(key, true);
+            var valueHeight = value.propertyType switch
+            {
+                SerializedPropertyType.String => EditorGUILayoutExtensions.GetTextAreaHeight(value, EditorGUIUtility.currentViewWidth),
+                _ => EditorGUI.GetPropertyHeight(value, true)
+            };
+
+            // Key and value property heights and twice the spacing (above and below the value property)
+            return keyHeight + valueHeight + (EditorGUIUtility.standardVerticalSpacing * 2);
         }
 
         private static void OnListOnAddCallback(ReorderableList reorderableList, SerializedDictionaryObject serializedDictionary)
@@ -223,8 +243,15 @@ namespace Utilities.Extensions.Editor
         #endregion List Callbacks
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-            => reorderableListCache.TryGetValue(property.propertyPath, out var list)
-                ? list.GetHeight()
-                : base.GetPropertyHeight(property, label);
+        {
+            if (reorderableListCache.TryGetValue(property.propertyPath, out var list))
+            {
+                return list.GetHeight();
+            }
+            else
+            {
+                return base.GetPropertyHeight(property, label);
+            }
+        }
     }
 }
